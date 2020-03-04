ba522ae77497bef300534acadd13365851997519
viktor <vitek230290@gmail.com>
2020-03-04 19:16:28 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -4,8 +4,90 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
-}
+    let arrElements = [];
+    if (/\s/.test(expr)) {
+      arrElements = expr.split(/\s+/);
+    } else {
+      arrElements = expr.split('');
+    }
+    let output = [];
+    let operatorStack = [];
+    let precedence = {
+      '+': 2,
+      '-': 2,
+      '*': 3,
+      '/': 3
+    };
+    arrElements.map(el => {
+      if (/\d+/gi.test(el)) {
+        output.push(el);
+      } else if (/[\-+*/]/gi.test(el)) {
+        while (
+          precedence[operatorStack[operatorStack.length - 1]] >= precedence[el] &&
+          operatorStack[operatorStack.length - 1] !== '('
+        ) {
+          output.push(operatorStack.pop());
+        }
+        operatorStack.push(el);
+      } else if (el === '(') {
+        operatorStack.push(el);
+      } else if (el === ')') {
+        while (operatorStack[operatorStack.length - 1] !== '(') {
+          output.push(operatorStack.pop());
+        }
+        if (operatorStack[operatorStack.length - 1] === '(') {
+          operatorStack.pop();
+        }
+      }
+    });
+    while (operatorStack.length !== 0) {
+      output.push(operatorStack.pop());
+    }
+  
+    function changeNums(num) {
+      result.pop();
+      result.pop();
+      result.push(num);
+    }
+    let result = [];
+    let divisionByZero = output.some(el => {
+      let calculatedNum;
+      if (/\d+/gi.test(el)) {
+        result.push(el);
+      }
+      switch (el) {
+        case '*':
+          calculatedNum = result[result.length - 2] * result[result.length - 1];
+          changeNums(calculatedNum);
+          break;
+        case '/':
+          if (result[result.length - 1] == 0) {
+            return 1;
+          }
+          calculatedNum = result[result.length - 2] / result[result.length - 1];
+          changeNums(calculatedNum);
+          break;
+        case '-':
+          calculatedNum = result[result.length - 2] - result[result.length - 1];
+          changeNums(calculatedNum);
+          break;
+        case '+':
+          calculatedNum =
+            parseFloat(result[result.length - 2]) +
+            parseFloat(result[result.length - 1]);
+          changeNums(calculatedNum);
+          break;
+      }
+    });
+  
+    if (isNaN(result[0])) {
+      throw 'ExpressionError: Brackets must be paired';
+    } else if (divisionByZero) {
+      throw 'TypeError: Division by zero.';
+    } else {
+      return result[0];
+    }
+  }
 
 module.exports = {
     expressionCalculator
